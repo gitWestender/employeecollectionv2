c898abd27f6a7a650ec77629fb42414f1b4a11f0
gitWestender <javaxwestender@gmail.com>
2023-12-08 10:53:57 +0300

    complete model Employee

--- src/main/java/com/javalearn/employeecollectionv2/models/Employee.java
+++ src/main/java/com/javalearn/employeecollectionv2/models/Employee.java
@@ -1,4 +1,74 @@
 package com.javalearn.employeecollectionv2.models;
 
+import java.util.Objects;
+
+import static org.springframework.util.StringUtils.capitalize;
+
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private Double salary;
+    private int department;
+
+    public Employee(String firstName, String lastName, Double salary, int department) {
+        this.firstName = capitalize(firstName.toLowerCase());
+        this.lastName = capitalize(lastName.toLowerCase());
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(salary, employee.salary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, salary, department);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", department=" + department +
+                '}';
+    }
 }
