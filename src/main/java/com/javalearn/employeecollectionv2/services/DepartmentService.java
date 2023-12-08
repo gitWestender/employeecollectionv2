a8f2260c8e9dd3b620d767ed1e8be79ed2eaa5c7
gitWestender <javaxwestender@gmail.com>
2023-12-08 12:03:43 +0300

    add DepartmentService methods

--- src/main/java/com/javalearn/employeecollectionv2/services/DepartmentService.java
+++ src/main/java/com/javalearn/employeecollectionv2/services/DepartmentService.java
@@ -1,4 +1,39 @@
 package com.javalearn.employeecollectionv2.services;
 
-public class DepartmentService {
+import com.javalearn.employeecollectionv2.interfaces.IDepartmentService;
+import com.javalearn.employeecollectionv2.models.Employee;
+
+import java.util.List;
+import java.util.Map;
+
+public class DepartmentService implements IDepartmentService {
+    private final EmployeeService empService;
+    public DepartmentService(EmployeeService empService) {
+        this.empService = empService;
+    }
+
+    @Override
+    public Employee findMaxSalaryOnDepart(int department) {
+        return null;
+    }
+
+    @Override
+    public Employee findMinSalaryOnDepart(int department) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> findAllOnDepart(int department) {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, List<Employee>> findAll() {
+        return null;
+    }
+
+    @Override
+    public Double sumOfSalary(int department) {
+        return null;
+    }
 }
