c5fc6bb39355a0702fe602c551f146696fe23b25
gitWestender <javaxwestender@gmail.com>
2023-12-08 10:34:11 +0300

    initial commit

---
+++ src/main/java/com/javalearn/employeecollectionv2/Employeecollectionv2Application.java
@@ -0,0 +1,13 @@
+package com.javalearn.employeecollectionv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeecollectionv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeecollectionv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalearn/employeecollectionv2/Employeecollectionv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearn.employeecollectionv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeecollectionv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
