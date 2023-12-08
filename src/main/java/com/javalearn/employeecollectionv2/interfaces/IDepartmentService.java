68bd9b509b1f3d79cbac4133ca92dd56cf326edf
gitWestender <javaxwestender@gmail.com>
2023-12-08 11:52:26 +0300

    complete IDepartmentService interface

--- src/main/java/com/javalearn/employeecollectionv2/interfaces/IDepartmentService.java
+++ src/main/java/com/javalearn/employeecollectionv2/interfaces/IDepartmentService.java
@@ -1,4 +1,14 @@
 package com.javalearn.employeecollectionv2.interfaces;
 
+import com.javalearn.employeecollectionv2.models.Employee;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IDepartmentService {
+    Employee findMaxSalaryOnDepart(int department);
+    Employee findMinSalaryOnDepart(int department);
+    List<Employee> findAllDepart(int department);
+    Map<Integer, List<Employee>> findAll();
+    Double sumOfSalary(int department);
 }
